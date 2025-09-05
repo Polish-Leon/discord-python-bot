962aeaa63e763c5d01dfd21fbab8091683fa883e
Polish Leon <ser.adress@pm.me>
2025-09-05 18:18:09 +0200

    Main intrruduction

--- main.py
+++ main.py
@@ -1,3 +1,98 @@
-print("Hello mobile!")
+import discord
+import os
+from discord.ext import commands
 
-# this is a comment from mobile
+description = '''Discord bot for a SCP TRP (Text role-play) server.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = commands.Bot(command_prefix='s?', description=description, intents=intents)
+
+if type(bot) != commands.bot:
+    print("Failed to fetch a bot!")
+    exit
+
+# level 3 - Owner
+# level 2 - Manager
+# level 1 - Mod
+# level 0 - Guest
+
+Authorizations = {bot.owner_id:3}
+owner_id = bot.owner_id
+
+def check_access(UserId:int):
+    accesslevel = Authorizations.get(UserId)
+
+    if accesslevel != None:
+        return accesslevel
+    else:
+        return 0
+
+@bot.event
+async def on_ready():
+    # Tell the type checker that User is filled up at this point
+    assert bot.user is not None
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+    await bot.change_presence(activity=discord.Game("Site systems online."),status=discord.Status.online)
+
+@bot.command()
+async def set_status(ctx:commands.Context, message):
+    if ctx.author.id != owner_id:
+        ctx.send(content="**[ACCES DENIED]:** Your access is not high enough.",delete_after=30.0)
+        return
+    ctx.send(content="**[ACCES GRANTED]:** Changing status.",delete_after=30.0)
+    print(message)
+
+@bot.command()
+async def get_categories(ctx:commands.Context):
+    i = 0
+    text = ""
+    for category in ctx.guild.categories:
+        i += 1
+        if len(text) + len(f"{category.name}:{category.id}\n") > 2000:
+            await ctx.send(text)
+            text = f"{category.name}:{category.id}\n"
+        else:
+            text += f"{category.name}:{category.id}\n"
+    await ctx.send(text)
+
+@bot.command()
+async def get_roles(ctx:commands.Context):
+    i = 0
+    text = ""
+    for role in ctx.guild.roles:
+        i += 1
+        if len(text) + len(f"{role.name}:{role.id}\n") > 2000:
+            await ctx.send(text)
+            text = f"{role.name}:{role.id}\n"
+        else:
+            text += f"{role.name}:{role.id}\n"
+    await ctx.send(text)
+
+@bot.command()
+async def test(ctx:commands.Context):
+    await ctx.send(type(ctx))
+    async for channel in type():
+        await ctx.send(f"the bot is in server: {ctx.guild}")
+
+@bot.command()
+async def stop_living(ctx):
+    ctx.send("Going off to retire (call me when needed)!")
+    await bot.close()
+
+@bot.command()
+async def kill(ctx:commands.Context, user:discord.User):
+    await ctx.send("*Pulls out a gun.*")
+    await ctx.send(f"*Shoots {user.mention}*")
+
+@bot.command()
+async def respond_to_me_please(ctx:commands.Context):
+    await ctx.reply("no fuck you!")
+    
+
+bot.run(BOT_TOKEN)
