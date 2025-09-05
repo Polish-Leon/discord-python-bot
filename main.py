cf495730604766f200ab886b31d18161c6a406ff
Polish Leon <ser.adress@pm.me>
2025-09-05 18:34:08 +0200

    test

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ bot = commands.Bot(command_prefix='s?', description=description, intents=intents
 
 Authorizations = {bot.owner_id:3}
 owner_id = bot.owner_id
-
+print(owner_id)
 def check_access(UserId:int):
     accesslevel = Authorizations.get(UserId)
      
