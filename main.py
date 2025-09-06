443fc38ced7d5c8bdc86c6b81aa1b1e2b4870254
Polish Leon <ser.adress@pm.me>
2025-09-06 10:49:26 +0200

    test

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def on_ready():
     
 
 
-@bot.tree.command(name="set_status",description="Set the status of the site, requires level 2 auth.")
+@bot.command()
 async def set_status(ctx:commands.Context, *message):
     if not await check_access(ctx,2): return
     await ctx.send(content="**[ACCESS GRANTED]:** Changing status.",delete_after=30.0)
@@ -73,7 +73,7 @@ async def set_status(ctx:commands.Context, *message):
         text = text + s + " "
     await bot.change_presence(activity=discord.CustomActivity(text),status=discord.Status.online)
 
-@bot.tree.command(name="intercom",description="Broadcast a message on the intercom, requires level 2 auth.")
+@bot.command()
 async def intercom(ctx:commands.Context,*message):
     if not await check_access(ctx,2): return
     await ctx.send(content="**[ACCESS GRANTED]:** Begining transmission.",delete_after=30.0)
